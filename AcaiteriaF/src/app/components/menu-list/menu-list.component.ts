79f3db03cce89b1eef5fecab35a65866ee952199
Saulo <saulosantos229@gmail.com>
2023-09-30 12:59:42 -0300

    Adicionando alguns components e services. Sem nenhuma criação neles.

---
+++ AcaiteriaF/src/app/components/menu-list/menu-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-list',
+  templateUrl: './menu-list.component.html',
+  styleUrls: ['./menu-list.component.css']
+})
+export class MenuListComponent {
+
+}
